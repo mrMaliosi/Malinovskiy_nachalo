b8de6c6e5db905b0cc29e52efcd0b12f0260da44
Малиновский Денис <d.malinovskii@g.nsu.ru>
2023-09-14 21:47:16 +0700

    lab 0 100% работающая

--- nachalo/cmake-build-debug/cmake_install.cmake
+++ nachalo/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Владимир/Desktop/Универ/2 курс/nachalo
+# Install script for directory: C:/Users/Владимир/Desktop/Универ/2 курс/ООП/Malinovskiy_nachalo/nachalo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Владимир/Desktop/Универ/2 курс/nachalo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Владимир/Desktop/Универ/2 курс/ООП/Malinovskiy_nachalo/nachalo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
