c4dd47f19ca266e60619978529ffba3c929a9647
Малиновский Денис <d.malinovskii@g.nsu.ru>
2023-11-24 09:28:54 +0700

    lab2 1.0

---
+++ lab2_game_of_life/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Владимир/Desktop/Универ/2 курс/ООП/Malinovskiy_nachalo/lab2_game_of_life/googletest
+# Build directory: C:/Users/Владимир/Desktop/Универ/2 курс/ООП/Malinovskiy_nachalo/lab2_game_of_life/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
